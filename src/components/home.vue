9fe0b890d79ccd35805d8da4f7766de6a73465ab
Jonalyn Razon <36467114+Ipsaeju@users.noreply.github.com>
2020-11-05 16:30:20 -0800

    Begin html skeleton

--- src/components/home.vue
+++ src/components/home.vue
@@ -1,4 +1,15 @@
 <template>
+    <div id="title">
+        <h1>Welcome</h1>
+        <div id="box1">
+            <h2>Box 1</h2>
+        </div>
+        <div id="box2">
+            <h2>Box 2</h2>
+        </div>
+        
+    </div>
+    
     
 </template>
 
