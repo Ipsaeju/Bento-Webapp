d6542052def2cdd0be971099e0599d52db77044f
Jonalyn Razon <36467114+Ipsaeju@users.noreply.github.com>
2020-11-09 17:09:49 -0800

    About page skeleton

---
+++ src/components/about.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="title">
+        <h1>Menu</h1>
+        <div class="box1">
+            <h2>Box 1</h2>
+            <div class="boxtext">
+                lorem ipsum
+            </div>
+        </div>
+    </div>
+    
+    
+</template>
+
+<script>
+export default {
+    name: 'About',
+    
+}
+</script>
+
+<style>
+    .box1{
+        box-sizing: content-box;
+        font-family: "Noto Sans";
+        font-size: 30px;
+    }
+    .boxtext{
+        font-family: "Noto Sans";
+        font-size: 14px;
+        text-align: justify;
+    }
+</style>
